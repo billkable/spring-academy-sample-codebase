eaf282fe54fda0906b71c6486a6da40c97b8080d
Bill Kable <bkable@gmail.com>
2023-03-01 23:28:05 -0600

    <rest-controller-start> add rest controller test

--- src/test/java/spring/academy/sample/SampleApplicationTests.java
+++ src/test/java/spring/academy/sample/SampleApplicationTests.java
@@ -1,13 +1,27 @@
 package spring.academy.sample;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SampleApplicationTests {
+	@Autowired
+	private TestRestTemplate restTemplate;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Disabled
+	@Test
+	void shouldReturnHelloWorld() {
+		String body = this.restTemplate.getForObject("/hello", String.class);
+		
+		assertThat(body).isEqualTo("Hello World");
+	}
 }
