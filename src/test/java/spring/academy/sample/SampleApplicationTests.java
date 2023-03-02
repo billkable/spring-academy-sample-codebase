9fa87dbaccbd7fa7eb84df3fd5128e726b70a999
Bill Kable <bkable@gmail.com>
2023-03-01 23:13:56 -0600

    <initialzr-solution> generate initialzr project

---
+++ src/test/java/spring/academy/sample/SampleApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.academy.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
